99d09e5d370c87d73b04d1a0d36cb616344d0b93
Vibhor <bijoy.vibhor19@gmail.com>
2017-06-05 05:10:53 +0530

    Create Vextor.java

---
+++ Vextor.java
@@ -0,0 +1,14 @@
+import java.util.*;      
+class TestVector1{      
+ public static void main(String args[]){      
+  Vector<String> v=new Vector<String>();//creating vector  
+  v.add("umesh");//method of Collection  
+  v.addElement("irfan");//method of Vector  
+  v.addElement("kumar");  
+  //traversing elements using Enumeration  
+  Enumeration e=v.elements();  
+  while(e.hasMoreElements()){  
+   System.out.println(e.nextElement());  
+  }  
+ }      
+}      
