77fc6055be2fdc9f6f3c345d12fc22d164adfe94
Vibhor <bijoy.vibhor19@gmail.com>
2017-06-05 12:52:02 +0530

    Create MainTree.java

---
+++ MainTree.java
@@ -0,0 +1,12 @@
+import java.util.TreeSet;
+
+public class Main {
+public static void main(String[] args ){
+	TreeSet<String> clothes = new TreeSet<String>();
+	clothes.add("Tshirt");
+	clothes.add("Jeans");
+	clothes.add("Coat");
+	System.out.println(clothes);
+	
+}
+}
