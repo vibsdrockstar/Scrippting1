f1fc62a2d1945305a500f27ded092d432e710c82
Vibhor <bijoy.vibhor19@gmail.com>
2017-06-07 14:19:16 +0530

    Create MyData2.java

---
+++ MyData2.java
@@ -0,0 +1,40 @@
+package JXLSProj;
+
+public class MyData
+{
+  private String key = "";
+  private String value = "";
+
+
+  public MyData(String key, String value)
+  {
+    this.key = key;
+    this.value = value;
+  }
+
+
+  public String getKey()
+  {
+    return key;
+  }
+
+
+  public void setKey(String key)
+  {
+    this.key = key;
+  }
+
+
+  public String getValue()
+  {
+    return value;
+  }
+
+
+  public void setValue(String value)
+  {
+    this.value = value;
+  }
+  
+  
+}
