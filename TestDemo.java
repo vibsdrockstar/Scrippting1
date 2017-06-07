914c859a92ebc3d2e82f9999f479dd83cc710411
Vibhor <bijoy.vibhor19@gmail.com>
2017-06-07 10:42:54 +0530

    Create TestDemo.java

---
+++ TestDemo.java
@@ -0,0 +1,16 @@
+package JXLSProj;
+
+public class test1
+{
+
+  private String pages = "";
+  private String flows = "";
+  private String pvs = "";
+  
+  test1(String pages, String flows, String pvs)
+  {
+    this.pages = pages;
+    this.flows = flows;
+    this.pvs = pvs;
+  }
+}
