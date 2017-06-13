108d580de2f60c4a5f945a55f780bc6864cd208c
Vibhor <bijoy.vibhor19@gmail.com>
2017-06-13 17:40:47 +0530

    Create KeyLogic.java

---
+++ KeyLogic.java
@@ -0,0 +1,14 @@
+Object.keys(data).forEach(function(k) {
+    if(k === "AVERAGE PAGE THINK TIME") {
+        System.out.println(data);
+    }
+    else if (k === "AVERAGE PAGE FAILURE RATE") {
+        System.out.println(data);
+    }
+    else if (k === "VUSERS") {
+        System.out.println(data);
+    }
+    else {
+     System.out.println("BLABLABLA")
+    }
+});
